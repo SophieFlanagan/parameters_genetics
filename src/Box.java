32b1fac12c78bf4ab098400dcc2b25a8676c796e
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-07-26 15:21:25 +0100

    feat: task 3 incomplete

--- src/Box.java
+++ src/Box.java
@@ -1,9 +1,19 @@
 public class Box<T> {
 
     private T storedValue;
+    private String name;
 
-    public Box(T storedValue) {
+    public Box(T storedValue, String name) {
         this.storedValue = storedValue;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public T getStoredValue(){
